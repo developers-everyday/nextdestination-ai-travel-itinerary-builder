d0c70b2e56705044f508b877a62c5888c7e21414
Rajat.Singh <rajat.singh@einfochips.com>
2025-12-28 23:57:34 +0530

    nextdestination: added mic

--- components/ItineraryDisplay.tsx
+++ components/ItineraryDisplay.tsx
@@ -422,9 +422,15 @@ const ItineraryBuilder: React.FC<Props> = ({
           {/* Search Box */}
           <div className="absolute top-6 left-8 right-8 z-20">
             <div className="bg-white/95 backdrop-blur-md p-1.5 rounded-xl shadow-xl shadow-slate-300/40 flex items-center gap-3 border border-slate-200">
-              <div className="w-10 h-10 bg-slate-100 rounded-lg flex items-center justify-center">
-                <div className="w-5 h-5 bg-slate-300 rounded-sm" />
-              </div>
+              <button
+                className="p-2 hover:bg-slate-100 rounded-lg text-slate-400 hover:text-indigo-600 transition-colors"
+                onClick={() => console.log("Start voice search")}
+                title="Voice Search"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
+                </svg>
+              </button>
               <input
                 type="text"
                 placeholder="Search places..."
